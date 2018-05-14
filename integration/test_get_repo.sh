d7c974f6e184f8866d7353d13afd8cb6ef05268d
Thomas Stringer <git@trstringer.com>
2018-05-14 19:14:42 -0400

    add get repos integration test

---
+++ integration/test_get_repo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "$(realpath "$0")")
+
+GIMME_BIN="$SCRIPT_PATH/../bin/gimme"
+COMMAND="get repos"
+
+# the get repos command should return a zero exit status
+if eval "$GIMME_BIN $COMMAND"; then
+    echo "$(date) - Success at '$GIMME_BIN $COMMAND'"
+else
+    echo "$(date) - Failed at '$GIMME_BIN $COMMAND'"
+    exit 1
+fi
+
+# get repos should return more than zero repos
+REPO_COUNT=$(eval "$GIMME_BIN $COMMAND" | wc -l)
+EXPECTED_REPO_COUNT=2
+if [[ $REPO_COUNT -lt $EXPECTED_REPO_COUNT ]]; then
+    echo "$(date) - '$GIMME_BIN $COMMAND' returned less than $EXPECTED_REPO_COUNT line(s) of output"
+    exit 1
+fi
+
+echo "$(date) - All tests passed"
+exit 0
