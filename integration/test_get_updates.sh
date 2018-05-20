e368437f08daac238a929d1f2794541e66d51663
Thomas Stringer <git@trstringer.com>
2018-05-20 09:27:53 -0400

    refactor get updates integration test to dump output on error for count

--- integration/test_get_updates.sh
+++ integration/test_get_updates.sh
@@ -14,10 +14,13 @@ else
 fi
 
 # get updates should return more than zero updates
-UPDATE_COUNT=$(eval "$GIMME_BIN $COMMAND" | wc -l)
+UPDATE_OUTPUT=$(eval "$GIMME_BIN $COMMAND")
+UPDATE_OUTPUT_COUNT=$(echo "$UPDATE_OUTPUT" | wc -l)
 EXPECTED_UPDATE_COUNT=2
-if [[ $UPDATE_COUNT -lt $EXPECTED_UPDATE_COUNT ]]; then
+if [[ $UPDATE_OUTPUT_COUNT -lt $EXPECTED_UPDATE_COUNT ]]; then
     echo "$(date) - '$GIMME_BIN $COMMAND' returned less than $EXPECTED_UPDATE_COUNT line(s) of output"
+    echo "Output:"
+    echo "$UPDATE_OUTPUT"
     exit 1
 fi
 
